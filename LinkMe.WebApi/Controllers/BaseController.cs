45006b8dccc23a55047db1b25462535b6ec9d22e
Filip Łapiński <f_lapinski@outlook.com>
2024-01-15 09:30:19 +0100

    Base and User Controllers implemented

---
+++ LinkMe.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkMe.WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        protected readonly ILogger _logger;
+        protected readonly IMediator _mediator;
+
+        public BaseController(ILogger logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+    }
+}
---
+++ LinkMe.WebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using LinkMe.Application.Logic.User;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkMe.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+        {
+            var createAccountResult = await _mediator.Send(model);
+            return Ok(createAccountResult);
+        }
+    }
+}
